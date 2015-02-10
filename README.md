6df0946493c5bfe251fda8f94b86675e5d85fa94
Bogdan Chadkin <trysound@yandex.ru>
2015-02-11 02:28:14 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# patch-layout
+Bootstrap like Layout System
+
+
+##License
+
+[The MIT License (MIT)](LICENSE)
+
+Copyright &copy; 2015 Bogdan Chadkin
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+	cssimport = require('gulp-cssimport'),
+	fs = require('fs'),
+	postcss = require('gulp-postcss'),
+	header = require('gulp-header'),
+	rename = require('gulp-rename'),
+	template = ['/*!',
+				' * <%= name %> <%= version %>',
+				' * <%= description %>',
+				' * <%= homepage %>',
+				' * ',
+				' * Released under the <%= license %> license',
+				' * Copyright (c) <%= new Date().getFullYear() %>, <%= author %>',
+				' */\n\n'].join('\n');
+
+gulp.task('default', function () {
+	var pkg = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+	return gulp.src('src/' + pkg.name + '.css')
+		.pipe(cssimport())
+		.pipe(postcss([
+			require('postcss-mixins')(),
+			require('postcss-custom-properties')(),
+			require('postcss-nested'),
+			require('postcss-calc')()
+		]))
+		.pipe(header(template, pkg))
+		.pipe(gulp.dest('dist'))
+		.pipe(postcss([
+			require('csswring')()
+		]))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('dist'));
+});
