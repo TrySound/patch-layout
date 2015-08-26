35f7cf99ebd973983bd819270e0018e10b42c789
Bogdan Chadkin <trysound@yandex.ru>
2015-08-26 14:20:19 +0300

    Minimized idea

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,16 @@
 var readFile = require('fs').readFileSync;
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
+var plugin = require('postcss').plugin;
 var header = require('gulp-header');
 var rename = require('gulp-rename');
 var pkg = JSON.parse(readFile('package.json', 'utf-8'));
 var template = ['/*!',
 				' * <%= name %> <%= version %>',
-				' * <%= description %>',
 				' * <%= homepage %>',
-				' * ',
-				' * Released under the <%= license %> license',
-				' * Copyright (c) <%= new Date().getFullYear() %>, <%= author %>',
+				' * Copyright <%= new Date().getFullYear() %> <%= author %>',
 				' */\n\n'].join('\n');
+
 
 gulp.task('default', function (done) {
 	return gulp.src([
@@ -20,16 +19,16 @@ gulp.task('default', function (done) {
 		.pipe(postcss([
 			require('precss'),
 			require('postcss-calc'),
-			require('postcss-clearfix')
+			require('postcss-clearfix'),
+			plugin('postcss-selector-fmt', function () {
+				return function (css) {
+					css.eachRule(function (rule) {
+						rule.selector = rule.selectors.join(',\n');
+					});
+				};
+			})
 		]))
 		.on('error', done)
 		.pipe(header(template, pkg))
-		.pipe(gulp.dest('dist'))
-		.pipe(postcss([
-			require('cssnano'),
-			require('css-mqpacker')
-		]))
-		.on('error', done)
-		.pipe(rename({suffix: '.min'}))
 		.pipe(gulp.dest('dist'));
 });
